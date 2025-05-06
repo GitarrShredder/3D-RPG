24b324b6f1eba9fa635be3c18b8962d8c0dc64c7
JohanWeigel <johanweigel@mac.com>
2025-05-06 16:24:44 +0200

    Camera rotation pivot and clamp

--- Player/player.gd
+++ Player/player.gd
@@ -11,6 +11,14 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var _look := Vector2.ZERO
 
 @export var mouse_sensitivity: float = 0.00075
+@export var min_boundary: float = -60
+@export var max_boundary: float = 10
+
+@onready var horizontal_pivot: Node3D = $HorizontalPivot
+@onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+
+
+
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -46,8 +54,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			_look = -event.relative * mouse_sensitivity
-			print(_look)
+		
 
-func frame_camera_rotation() -> void:
-	$SpringArm3D.rotate_y(_look.x)
+func frame_camera_rotation() -> void: 
+	horizontal_pivot.rotate_y(_look.x)
+	vertical_pivot.rotate_x(_look.y)
+	
+	vertical_pivot.rotation.x = clampf(
+		vertical_pivot.rotation.x, 
+		deg_to_rad(min_boundary),
+		deg_to_rad(max_boundary)
+		)
+	
+	$SpringArm3D.global_transform = vertical_pivot.global_transform
 	_look = Vector2.ZERO
